6327b1040d0730c118e6a8ac1b15166f19de75ed
Paul Jordaan <pdjordaan@gmail.com>
2017-09-17 20:30:46 +0200

    Update ReactNativeAudioStreaming.m
    Fix for `redefinition of 'RCTMethodInfo'`

--- ios/ReactNativeAudioStreaming.m
+++ ios/ReactNativeAudioStreaming.m
@@ -1,5 +1,10 @@
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventDispatcher.h>
+#else
 #import "RCTBridgeModule.h"
 #import "RCTEventDispatcher.h"
+#endif
 
 #import "ReactNativeAudioStreaming.h"
 
